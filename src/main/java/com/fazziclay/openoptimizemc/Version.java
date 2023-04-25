62c6ce011269907a4e0396e7e9a00dc4dc0afab4
fazziclay <fazziclay@gmail.com>
2023-04-25 17:27:25 +0300

    merge version to 1.0.6 (build 17)

--- src/main/java/com/fazziclay/openoptimizemc/Version.java
+++ src/main/java/com/fazziclay/openoptimizemc/Version.java
@@ -1,7 +1,7 @@
 package com.fazziclay.openoptimizemc;
 
 public class Version {
-    public static final boolean DEVELOPMENT = true;
-    public static final int BUILD = 16;
-    public static final String NAME = "1.0.6 pre-test1";
+    public static final boolean DEVELOPMENT = false;
+    public static final int BUILD = 17;
+    public static final String NAME = "1.0.6";
 }
